5b9b674e50e4e5fdb5af5287defc7b1e3aac9eb0
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2019-08-28 20:35:09 -0400

    created project

--- assignment_1/src/assignment_1/HelloWorld.java
+++ assignment_1/src/assignment_1/HelloWorld.java
@@ -1,11 +1,15 @@
-//Daniela
+/*	
+ 	Daniela Cruz Falquez
+	Assignment 1
+	08/28/2019
+*/
+
 package assignment_1;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		
-	}
 
+	}
 }
